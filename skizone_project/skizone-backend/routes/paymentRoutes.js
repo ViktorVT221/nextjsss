df36c4a39aca6874d3ef3ae6c5a0b34aa9f959ad
ViktorVT221 <vt221_kvv@student.ztu.edu.ua>
2025-09-01 12:32:11 +0300

    Delete skizone_project directory

--- skizone_project/skizone-backend/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
-const { confirmOrder } = require('../controllers/paymentController');
-
-router.post('/confirm-order', authMiddleware, confirmOrder);
-
-module.exports = router;
